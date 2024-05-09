18e3d6493d83aa718f1d4f4c59852eeab7b4aa8e
Sebastian Post <115038252+ZynCitrus@users.noreply.github.com>
2024-05-09 17:40:27 +0200

    Install, initialize and grejsaker.

---
+++ src/fbconfig/fbconfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVgSdKuiMrsVi29y76rGwzauao1A9yHVw",
+  authDomain: "modernmoviedatabase.firebaseapp.com",
+  projectId: "modernmoviedatabase",
+  storageBucket: "modernmoviedatabase.appspot.com",
+  messagingSenderId: "893102822451",
+  appId: "1:893102822451:web:7968316a5a8d48f0b53f46",
+  measurementId: "G-2VS4XJGY2Q",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
