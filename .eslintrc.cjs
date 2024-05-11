39ea7f73f4c8be67146e6c7219653d0d15edcb06
Sebastian Post <115038252+ZynCitrus@users.noreply.github.com>
2024-05-11 14:52:02 +0200

    Workflow json's

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "package.json"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "package.json", "firebase.json"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
