bc04ca5e7c66001d4918ddd38c6f8a26c0e13d4e
Sebastian Post <115038252+ZynCitrus@users.noreply.github.com>
2024-08-20 22:23:50 +0200

    Navlink på titel. Ger mig för inlämning. Fixar design flaws i morgon.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../design/Navbar.scss";
 import Navlinks from "./Navlinks";
+import { useNavigate, NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <div className="Navbar">
-        <p className="navbarTitle">Modern Movie Database</p>
+        {" "}
+        <NavLink exact={true.toString()} to="/">
+          <p className="navbarTitle">Modern Movie Database</p>
+        </NavLink>
         <Navlinks />
       </div>
     </>
